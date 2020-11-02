55f666b82bf3d491179323b63645c56b3f20235e
Uriel Romero <urome23@gmail.com>
2020-11-01 19:15:45 -0600

    first commit

--- CarRentalFareCalculator/src/Vehicle.java
+++ CarRentalFareCalculator/src/Vehicle.java
@@ -1,6 +1,5 @@
 public class Vehicle {
 
-
     //characteristics of each vehicle
      int range;
      //using all numbers on vin for simplicity. A method can be implemented to turn char/string to numbers
@@ -20,7 +19,66 @@ public class Vehicle {
          this.basePrice = basePrice;
          this.color = color;
          this.mfgName = mfgName;
-
      }
 
+     // constructor for no Default Color
+    public Vehicle(int range, int vin, int basePrice, String mfgName){
+
+        this.range = range;
+        this.vin = vin;
+        this.basePrice = basePrice;
+        this.mfgName = mfgName;
+    }
+
+
+    public void displayInfo(){
+
+        System.out.println("\nManufacture Name: " + mfgName);
+        System.out.println("Range: " + range);
+        System.out.println("VIN: " + vin);
+        System.out.println("Base Price: $" + basePrice);
+        System.out.println("Color: " + color);
+
+    }
+
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(int basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMfgName() {
+        return mfgName;
+    }
+
+    public void setMfgName(String mfgName) {
+        this.mfgName = mfgName;
+    }
 }
--- CarRentalFareCalculator/src/VehicleRentalsTest.java
+++ CarRentalFareCalculator/src/VehicleRentalsTest.java
@@ -1,7 +1,13 @@
 public class VehicleRentalsTest {
 
 
-public static void main (String args []){
+    public static void main (String args []) {
 
+        Vehicle newst = new Vehicle();
+        Vehicle audi = new Vehicle(200, 332324343, 45000, "Blue", "Audi");
+
+
+        newst.displayInfo();
+        audi.displayInfo();
     }
 }
